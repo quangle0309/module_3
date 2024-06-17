dc248cb2dd595702db0813a687a0e7d6c40320c4
quangle0309 <quang9999xx.1@gmail.com>
2024-06-17 09:36:17 +0700

    add new folder

---
+++ ss1_relational_database/student_management.sql
@@ -0,0 +1,22 @@
+create database demo;
+
+use demo;
+
+create table Student (
+id int,
+name varchar(200),
+age int,
+country varchar(50)
+);
+
+create table Class (
+id int,
+name varchar(200)
+);
+
+create table Teacher (
+id int,
+name varchar(200),
+age int,
+country varchar(50)
+)
